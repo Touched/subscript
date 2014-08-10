ec0ca4fefb481391a701aed2d6b14d379f72c2e1
Touched <Touched@users.noreply.github.com>
2014-08-10 10:01:34 +0200

    Added timing test to the CLI

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import subscript.compile
 import argparse
+import time
 
 parser = argparse.ArgumentParser(description='Pokescript compiler.')
 
@@ -10,8 +11,20 @@ parser.add_argument('--offset', metavar='offset', dest='offset', type=int, defau
 
 args = parser.parse_args()
 
-c = subscript.compile.Compile(args.script.read(), args.offset + 0x8000000)
+times = []
 
+tests = 100
+for i in range(tests):
+    start_time = time.process_time()
+    c = subscript.compile.Compile(args.script.read(), args.offset + 0x8000000)
+    end_time = time.process_time()
+    times.append(end_time - start_time)
+
+    # Seek to beginning of file to redo test
+    args.script.seek(0)
+
+mean = sum(times) / len(times)
+print('Compiled {tests} times, mean compile time is {mean}'.format(tests=tests, mean=mean))
 c.output()
 
 data = c.bytecode()
