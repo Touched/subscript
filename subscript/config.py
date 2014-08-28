9200460b4e3fbca7f893ab27b6616635358b802f
Touched <Touched@users.noreply.github.com>
2014-08-28 14:17:56 +0200

    Rom config

--- subscript/config.py
+++ subscript/config.py
@@ -1,21 +1,46 @@
 import json
 import os
 
-class Config(object):
+class RomConfig(object):
     '''
-    Load settings file. Uses a hard-coded search path to look for the config file.
-    Everything reading settings must use this file, so that the mechanism can
-    be easily changed.
+    Load settings file for a given ROM.
+    Everything reading settings must use this class, so that the mechanism can
+    be easily changed. Caches lookups to speed up item retrieval.
     '''
 
-    user = os.path.expanduser('~')
-    cwd = os.getcwd()
-
-    searchpath = [cwd]
-
-    def __init__(self, path=None):
+    def __init__(self):
         '''
         Constructor
         '''
-        with open(path) as file:
+        with open('../config/roms.json') as file:
             self.config = json.load(file)
+
+        self.cache = {}
+
+    def __getitem__(self, index):
+        if index in self.cache:
+            return self.cache[index]
+        elif index in self.config:
+            out = self.config[index]
+
+            child = self.config[index]
+            while child['inherits']:
+                parent = self.config[child['inherits']]
+
+                # Add all parent keys to the output, unless they already exist
+                for key in parent:
+                    if key not in out:
+                        out[key] = parent[key]
+
+                # Check if parent inherited anything
+                child = parent
+
+            # Cache item for faster lookup next time
+            self.chache[index] = out
+            return out
+        else:
+            # TODO: Raise a "NotSupported" exception
+            raise IndexError("Rom not supported")
+
+c = RomConfig()
+c['AXVE']
